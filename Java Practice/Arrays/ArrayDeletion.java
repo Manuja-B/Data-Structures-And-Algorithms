379d8897a986a3ed9e991d26fbc772ff84aa8dd8
msbandal <msbandal@iu.edu>
2019-11-19 18:07:05 -0500

    Create ArrayDeletion.java
    Delete the element of array at given position

---
+++ Java Practice/Arrays/ArrayDeletion.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class ArrayDeletion {
+	public static int[] removeTheElement(int[] arr, 
+            int index) 
+{ 
+
+// If the array is empty 
+// or the index is not in array range 
+// return the original array 
+if (arr == null
+|| index < 0
+|| index >= arr.length) { 
+
+return arr; 
+} 
+
+// Create another array of size one less 
+int[] anotherArray = new int[arr.length - 1]; 
+
+// Copy the elements except the index 
+// from original array to the other array 
+for (int i = 0, k = 0; i < arr.length; i++) { 
+
+// if the index is 
+// the removal element index 
+if (i == index) { 
+continue; 
+} 
+
+// if the index is not 
+// the removal element index 
+anotherArray[k++] = arr[i]; 
+} 
+
+// return the resultant array 
+return anotherArray; 
+} 
+
+// Driver Code 
+public static void main(String[] args) 
+{ 
+
+// Get the array 
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+
+// Print the resultant array 
+System.out.println("Original Array: "+ Arrays.toString(arr)); 
+
+// Get the specific index 
+int index = 7; 
+
+// Print the index 
+System.out.println("Index to be removed: "
++ index); 
+
+// Remove the element 
+arr = removeTheElement(arr, index); 
+
+// Print the resultant array 
+System.out.println("Resultant Array: "+ Arrays.toString(arr)); 
+} 
+
+
+}
