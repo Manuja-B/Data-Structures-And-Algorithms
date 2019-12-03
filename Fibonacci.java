dc08905e585f38cb7278c3db8de75b14d5a85d11
Manuja Sanjay Bandal <56208082+Manuja-B@users.noreply.github.com>
2019-12-03 18:49:59 -0500

    Fibonacci Series

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main(String args[]) {
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of terms for fibonacci series");
+		n=sc.nextInt();
+		int a=0, b=1, sum=0;
+		for(int i=1; i<=n; i++) {
+			System.out.println(a);
+			sum=a+b;
+			a=b;
+			b=sum;	
+		}
+	}
+
+
+
+}
